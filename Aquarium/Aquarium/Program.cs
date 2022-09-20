57496e8dc6e2478bc1b0280304f992fa6696c655
Bundschuh, Jonas <jonas.bundschuh@tecalliance.net>
2022-09-20 16:13:25 +0200

    add aquarium

---
+++ Aquarium/Aquarium/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aquarium
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 20;
+            int y = 20;
+            string[,] aquarium = new string[x, y];
+            for (int j = 0; j < x; j++)
+            {
+                for(int i = 0; i<y; i++)
+                {
+                    if(j == 0)
+                    {
+                        aquarium[i, j] = "-";
+                    }
+                    else if(i == y-1){
+                        aquarium[i, j] = "|";
+                    }
+                    else
+                    {
+                        aquarium[i,j] = " ";
+                    }
+
+                    if(j == x-1)
+                    {
+                        aquarium[i, j] = "-";
+                    }
+                    
+                }
+            }
+
+            for (int j = 0; j < 20; j++)
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    Console.Write(aquarium[i,j]);
+                }    
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
